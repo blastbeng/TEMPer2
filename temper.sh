4b917a92b8c36f245614166402a1d272d4e5b49e
blastbeng <rodfval.fv@gmail.com>
2022-11-21 21:59:20 +0100

    Update temper.sh

--- temper.sh
+++ temper.sh
@@ -8,37 +8,27 @@
 #   
 #   find the HID device from the kernel msg via dmesg
 #   parse the line get HID device
-hidstr=$(dmesg 2>/dev/null | grep -E  -m 1 'Device.*1a86:e025')
-if [ -z "$hidstr" ]; then
-  echo "No TEMPer device found. Are you running as root?"
-else 
-  # find the postion of the "hidraw" string
-  hidpos=$(echo $hidstr | grep -b -o hidraw  | awk 'BEGIN {FS=":"}{print $1}')
+#get the hidraw device from the string
+hid=$(echo "/dev/hidraw1")
+exec 5<> $hid
+# send out query msg
+echo -e '\x00\x01\x80\x33\x01\x00\x00\x00\x00\c' >&5
+# get binary response
+#  OUT=$(dd count=1 bs=8 <&5 2>/dev/null | xxd -p)
+OUT=$(dd count=2 bs=8 <&5 2>/dev/null | xxd -p)
+# characters 5-8 is the device temp in hex x1000
+DHEX4=${OUT:4:4}
+DDVAL=$((16#$DHEX4))
+DCTEMP=$(bc <<< "scale=2; $DDVAL/100")
+#  echo "Device: $DCTEMP"
 
-  if [ -z "$hidpos" ]; then
-    echo "No TEMPer device found"
-  else
-    #get the hidraw device from the string
-    hid=$(echo "/dev/${hidstr:hidpos:7}")
-    exec 5<> $hid
-    # send out query msg
-    echo -e '\x00\x01\x80\x33\x01\x00\x00\x00\x00\c' >&5
-    # get binary response
-    #  OUT=$(dd count=1 bs=8 <&5 2>/dev/null | xxd -p)
-    OUT=$(dd count=2 bs=8 <&5 2>/dev/null | xxd -p)
-    # characters 5-8 is the device temp in hex x1000
-    DHEX4=${OUT:4:4}
-    DDVAL=$((16#$DHEX4))
-    DCTEMP=$(bc <<< "scale=2; $DDVAL/100")
-    #  echo "Device: $DCTEMP"
-  
-    # characters 20-23 is the probe temp in hex x1000
-    PHEX4=${OUT:20:4}
-    PDVAL=$((16#$PHEX4))
-    PCTEMP=$(bc <<< "scale=2; $PDVAL/100")
-    #  echo "Probe: $PCTEMP" 
+# characters 20-23 is the probe temp in hex x1000
+PHEX4=${OUT:20:4}
+PDVAL=$((16#$PHEX4))
+PCTEMP=$(bc <<< "scale=2; $PDVAL/100")
+#  echo "Probe: $PCTEMP" 
 
-    # Output the temperature in JSON format
-    echo "{ \"Device\" : \"$DCTEMP\" , \"Probe\" : \"$PCTEMP\" }" 
-  fi 
-fi
+# Output the temperature in JSON format
+#echo "{ \"Device\" : \"$DCTEMP\" , \"Probe\" : \"$PCTEMP\" }" 
+
+echo $PCTEMP
